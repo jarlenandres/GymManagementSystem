7bae97ba7b989b987298a82a501b8edf0bba2c0e
jarlenandres <jaarojas@gmai.com>
2026-05-21 16:01:22 -0500

    Creation of a Gym Management System project

---
+++ GymManagement.Domain/Entities/AuditBase.cs
@@ -0,0 +1,8 @@
+﻿namespace GymManagement.Domain.Entities;
+
+public abstract class AuditBase
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdateAt { get; set; }
+}
